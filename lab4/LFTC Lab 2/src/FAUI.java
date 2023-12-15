5163e6743e87e5f56051526af3405362d86972d0
916/SCHIOP ADRIAN-PETRU <adrianschiop2@gmail.com>
2023-12-15 12:20:18 +0200

    FA final

---
+++ lab4/LFTC Lab 2/src/FAUI.java
@@ -0,0 +1,63 @@
+
+
+import java.util.Objects;
+
+public class FAUI {
+    public static void main(String[] args) {
+        boolean ok = true;
+        var fa = new FA("./src/Resources/FA.in");
+        System.out.println("1. Print states");
+        System.out.println("2. Print alphabet");
+        System.out.println("3. Print output states");
+        System.out.println("4. Print initial state");
+        System.out.println("5. Print transitions");
+        System.out.println("6. Check word");
+        System.out.println("7. Get matching substring");
+        System.out.println("0. Exit");
+        while (true) {
+            System.out.println("> ");
+            var option = new java.util.Scanner(System.in).nextInt();
+            switch (option) {
+                case 1:
+                    fa.printStates();
+                    break;
+                case 2:
+                    fa.printAlphabet();
+                    break;
+                case 3:
+                    fa.printOutputStates();
+                    break;
+                case 4:
+                    fa.printInitialState();
+                    break;
+                case 5:
+                    fa.printTransitions();
+                    break;
+                case 6:
+                    var word = new java.util.Scanner(System.in).nextLine();
+                    System.out.println(fa.checkAccepted(word));
+                    break;
+                case 7:
+                    word = new java.util.Scanner(System.in).nextLine();
+                    var accepted = fa.getNextAccepted(word);
+                    if (Objects.equals(accepted, "")) {
+                        System.out.println("No matching substring");
+                    } else {
+                        System.out.println(accepted);
+                    }
+                    break;
+                case 0:
+                    ok = false;
+                    break;
+                default:
+                    System.out.println("Invalid option");
+
+
+            }
+            if (!ok) {
+                System.out.print("Bye Bye!!");
+                break;
+            }
+        }
+    }
+}
