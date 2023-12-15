fe9d1e9cae344974278730cfc0cbca1e74067458
916/SCHIOP ADRIAN-PETRU <adrianschiop2@gmail.com>
2023-12-15 12:33:03 +0200

    Grammar

---
+++ lab5/LFTClab5/Grammar.py
@@ -0,0 +1,71 @@
+class Grammar:
+    EPSILON = "epsilon"
+
+    def __init__(self):
+        self.non_term = []  # non-terminals
+        self.term = []  # terminals
+        self.start = ""  # starting symbol/ axiom
+        self.production = {}  # finite set of productions
+
+    def rebuild(self):
+        self.non_term = []
+        self.term = []
+        self.start = ""
+        self.production = {}
+
+    def __processLine(self, line: str, delimiter=' '):
+            elements = line.strip().split(delimiter)
+            return elements
+
+    def read_from_file(self, file_name: str):
+        self.rebuild()
+        with open(file_name) as file:
+            line = next(file)
+            self.non_term = self.__processLine(line)
+
+            line = next(file)
+            self.term = self.__processLine(line)
+
+            line = next(file)
+            self.start = self.__processLine(line)[0]
+
+            line = file.readline()
+            while line.strip() and ' -> ' not in line:
+                line = file.readline()
+
+            while line:
+                if ' -> ' in line:
+                    source, productions = line.split(" -> ")
+                    source = source.strip()
+                    for production in productions.split('|'):
+                        production = production.strip().replace('epsilon', Grammar.EPSILON).split()
+                        if source in self.production:
+                            self.production[source].append(production)
+                        else:
+                            self.production[source] = [production]
+                line = file.readline()
+
+
+    def check_cfg(self):
+        has_starting_symbol = False
+        for key in self.production.keys():
+            if key == self.start:
+                has_starting_symbol = True
+            if key not in self.non_term:
+                return False
+        if not has_starting_symbol:
+            return False
+
+        for production in self.production.values():
+            for rhs in production:
+                for value in rhs:
+                    if value not in self.non_term and value not in self.term and value != Grammar.EPSILON:
+                        return False
+        return True
+
+    def __str__(self):
+        result = "N = " + str(self.non_term) + "\n"
+        result += "E = " + str(self.term) + "\n"
+        result += "S = " + str(self.start) + "\n"
+        result += "P = " + str(self.production) + "\n"
+        return result
